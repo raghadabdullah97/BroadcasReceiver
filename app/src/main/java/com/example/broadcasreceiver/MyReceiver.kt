f998590418324343802658ca7d12902ca635025c
Raghad Abdullah <Raghad.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-12-01 18:51:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/broadcasreceiver/MyReceiver.kt
@@ -0,0 +1,24 @@
+package com.example.broadcasreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.telephony.TelephonyManager
+import android.widget.Toast
+
+class MyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+      val extras = intent.extras
+        extras?.let {
+
+            val state = it.getString(TelephonyManager.EXTRA_STATE)
+
+            Toast.makeText(context,state , Toast.LENGTH_SHORT).show()
+
+
+        }
+
+    }
+}
